285989e414626c3247233d766e18390f760ad691
Nican <nican132@gmail.com>
2014-02-19 14:48:59 -0500

    Initial commit.

---
+++ Assets/PolyMesh/Scripts/InitGame.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitGame : MonoBehaviour {
+	public GameObject rock;
+
+	// Use this for initialization
+	void Start () {
+
+		var rock2 = Instantiate (rock, new Vector3 (0, 10), Quaternion.identity) as GameObject;
+		var a = rock2.GetComponent<PolyMesh> ();
+
+		a.keyPoints.Clear ();
+		a.isCurve.Clear ();
+
+		for (float r = 0.0f; r <= Mathf.PI * 2; r += 0.1f) {
+			a.keyPoints.Add(new Vector3(Mathf.Cos(r), Mathf.Sin(r) * 2));
+			a.isCurve.Add(false);
+		}
+
+		a.BuildMesh ();
+
+		//var a = GameObject.Find ("Rock");
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
