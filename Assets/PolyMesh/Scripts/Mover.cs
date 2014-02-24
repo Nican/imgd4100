c0043b4bd8fba1accdee428596d466e860913043
Nican <nican132@gmail.com>
2014-02-24 10:30:19 -0500

    "Mover" script added

---
+++ Assets/PolyMesh/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public Rigidbody r;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		r.AddForce(0,-10,0);
+	}
+}
